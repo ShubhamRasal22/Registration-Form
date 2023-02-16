187350fc4b7c24f30d8313f1f048f167779925f5
shubhamvitthalrasal <shubhamrasal861@gmail.com>
2023-02-16 18:21:23 +0530

    modified app file

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 import express from 'express';
+import connectDB from './db/connectdb.js';
+import web from './routes/web.js'
 const app = express()
 const port  = process.env.PORT || '7675'
+const DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost:27017";
 
+
+// Database Connection
+connectDB(DATABASE_URL)
+
+// Set template engine
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({extended:true}));
+
+// Load Routes
+app.use('/',web)
 
 
 app.listen(port,()=>{
